fee702997b68535ed4f867552728a5b64bade5b5
khaled0915 <khaledsaifullah50956@gmail.com>
2023-09-24 22:03:13 +0600

    created the component that will nedded

---
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -0,0 +1,13 @@
+
+
+const AppliedJobs = () => {
+    return (
+        <div>
+
+            <h2> job i have applied </h2>
+            
+        </div>
+    );
+};
+
+export default AppliedJobs;
---
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+
+
+const ErrorPage = () => {
+    return (
+        <div>
+
+            <h3> Oppss !! </h3>
+
+            <Link to='/'> Go Back to Home </Link>
+            
+        </div>
+    );
+};
+
+export default ErrorPage;
